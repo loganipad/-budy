29d1ecb108b3e5e0e5686c7de562ac881ea03c64
Logan Thomas <loganipad@gmail.com>
2026-03-24 23:10:48 -0700

    Match google play badge proportions

--- footer-loader.js
+++ footer-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var didMountFooter = false;
-  var FOOTER_CACHE_KEY = 'budy_footer_html_v7';
+  var FOOTER_CACHE_KEY = 'budy_footer_html_v8';
 
   function mountFooter() {
     if (didMountFooter) return;
