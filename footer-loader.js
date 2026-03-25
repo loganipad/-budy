c780812a5ffc638494ec437fcdc04fd2c94417f0
Logan Thomas <loganipad@gmail.com>
2026-03-24 23:17:50 -0700

    Preserve landing scroll on refresh

--- footer-loader.js
+++ footer-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var didMountFooter = false;
-  var FOOTER_CACHE_KEY = 'budy_footer_html_v10';
+  var FOOTER_CACHE_KEY = 'budy_footer_html_v11';
 
   function mountFooter() {
     if (didMountFooter) return;
