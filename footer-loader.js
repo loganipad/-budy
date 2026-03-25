1f667325982e2f0f99ab7ae90231376361a13762
Logan Thomas <loganipad@gmail.com>
2026-03-24 22:57:41 -0700

    Redesign shared footer layout

--- footer-loader.js
+++ footer-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var didMountFooter = false;
-  var FOOTER_CACHE_KEY = 'budy_footer_html_v3';
+  var FOOTER_CACHE_KEY = 'budy_footer_html_v4';
 
   function mountFooter() {
     if (didMountFooter) return;
