74e3e2227ef5e7a41887901f3211fdf8be0467d0
Logan Thomas <loganipad@gmail.com>
2026-03-24 23:08:45 -0700

    Refine google play badge

--- footer-loader.js
+++ footer-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var didMountFooter = false;
-  var FOOTER_CACHE_KEY = 'budy_footer_html_v6';
+  var FOOTER_CACHE_KEY = 'budy_footer_html_v7';
 
   function mountFooter() {
     if (didMountFooter) return;
