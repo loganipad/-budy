44085ec6906ceac69361b41897e0a8ff09bd1b90
Logan Thomas <loganipad@gmail.com>
2026-03-24 23:16:01 -0700

    Compress footer spacing

--- footer-loader.js
+++ footer-loader.js
@@ -1,6 +1,6 @@
 (function () {
   var didMountFooter = false;
-  var FOOTER_CACHE_KEY = 'budy_footer_html_v8';
+  var FOOTER_CACHE_KEY = 'budy_footer_html_v10';
 
   function mountFooter() {
     if (didMountFooter) return;
